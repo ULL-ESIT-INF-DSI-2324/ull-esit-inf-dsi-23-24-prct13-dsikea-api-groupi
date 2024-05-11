4f8ef35a113d9075d5b1410ed425d17ee65a646a
Ines Garrote <alu0101512297@ull.edu.es>
2024-05-11 22:47:14 +0000

    RENDER

--- src/db/mongoose.ts
+++ src/db/mongoose.ts
@@ -1,5 +1,6 @@
 import { connect } from 'mongoose';
 
+/*
 connect('mongodb://127.0.0.1:27017/dsikea-api-rest')
   .then(() => {
     console.log('Connection to MongoDB server established');
@@ -7,13 +8,11 @@ connect('mongodb://127.0.0.1:27017/dsikea-api-rest')
   .catch(() => {
     console.log('Unable to connect to MongoDB server');
   });
+*/
 
-// Connectamos base de datos
-export const Render = () => {
-  connect(process.env.MONGODB_URL!).then(() => {
-    console.log('Connected to the database');
-  }).catch(() => {
-    console.log('Something went wrong when conecting to the database');
-    process.exit(-1);
-  });
+try {
+  await connect(process.env.MONGODB_URL!);
+  console.log('Connection to MongoDB server established');
+} catch (error) {
+  console.log(error);
 }
